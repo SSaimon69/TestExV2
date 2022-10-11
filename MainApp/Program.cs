d77cfcd1f6d8802e561f1e330a63e069a9d9d4fe
Semen Sova <semen.sova@atos.net>
2022-10-11 23:44:01 +0300

    Добавьте файлы проекта.

---
+++ MainApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using WordCalc;
+using System.Reflection;
+
+namespace MainApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime firstTime = DateTime.Now;
+            string filePath = "text.txt";
+
+            Type type = typeof(WordCalc.Calc);
+            var a = type.GetMethod("CalculateWord", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            Dictionary<string, int> map = (Dictionary<string, int>) a.Invoke(new Calc(), new object[] { filePath });
+
+            //Сортировка
+            var sort = map.OrderByDescending(x => x.Value);
+
+            //Вывод в файл
+            using (StreamWriter sw = File.CreateText("textOut.txt"))
+            {
+                foreach (var x in sort)
+                {
+                    sw.WriteLine(x.Key + " " + x.Value);
+                }
+            }
+
+            Console.WriteLine("Затрачено времени: " + (DateTime.Now - firstTime).TotalSeconds + " секунд");
+        }
+    }
+}
---
+++ WordCalc/Calc.cs
@@ -0,0 +1,25 @@
+﻿namespace WordCalc
+{
+    public class Calc
+    {
+        Dictionary<string, int> CalculateWord(string filePath)
+        {
+            Dictionary<string, int> map = new Dictionary<string, int>();
+            string[] separators = { ",", ".", " ", " ", ";", ":", "?", "!", "\"", "(", ")", "—", "–", "[", "]", "»", "«" };
+
+            using (StreamReader sr = File.OpenText(filePath))
+            {
+                string? s = String.Empty;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    string[] mas = s.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (string ma in mas)
+                        if (map.ContainsKey(ma)) map[ma]++;
+                        else map.Add(ma, 1);
+                }
+            }
+
+            return map;
+        }
+    }
+}
